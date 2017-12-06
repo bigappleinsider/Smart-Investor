863cd4896293c3c9dd95dec7609ee26667a1ccf6
Vlad Vinnikov <bigappleinsider@users.noreply.github.com>
2017-12-06 14:12:36 -0800

    pass entities only once

--- test2.js
+++ test2.js
@@ -7,62 +7,92 @@ var app = apiai(process.env.APIAI_TOKEN);
 
 module.exports = {
     init: false,
+    recognizeWithEntities: function(context, callback) {
+      console.log('recognizeWithEntities', this.init);
+      this.init = true;
+      var sessionId = "dsfhdfg";
+
+      var user_entities = [{
+          name: 'nickname',
+          extend: false,
+          entries: [
+              {
+                  value: 'College',
+                  synonyms: ['College']
+              },
+              {
+                  value: 'Retirement',
+                  synonyms: ['Retirement']
+              }
+          ]
+      }];
+
+      var user_entities_body = {
+          sessionId: sessionId,
+          entities: user_entities
+      };
+      //console.log('user_entities_body', user_entities_body);
+
+      var user_entities_request = app.userEntitiesRequest(user_entities_body);
+
+      user_entities_request.on('response', function(response) {
+          //console.log('User entities response: ');
+          //console.log(JSON.stringify(response, null, 4));
+
+          var request = app.textRequest(context.message.text, {sessionId: sessionId});
+
+          //console.log('sessionId check ', sessionId);
+          request.on('response', function(response) {
+              //console.log('Query response: ');
+              //console.log(JSON.stringify(response, null, 4));
+              callback(null, {
+                  intent: response.result.metadata.intentName,
+                  score: response.result.score,
+                  parameters: response.result.parameters
+              });
+          });
+
+          request.on('error', function(error) {
+              console.log(error);
+          });
+
+          request.end();
+      });
+
+      user_entities_request.on('error', function(error) {
+          console.log(error);
+      });
+
+      user_entities_request.end();
+
+    },
     recognize: function(context, callback) {
-console.log('context', context, this.init);
-var sessionId = "dsfhdfg";
+      if(!this.init) {
+        this.recognizeWithEntities(context, callback);
+      }
+      else {
+        var sessionId = "dsfhdfg";
 
-var user_entities = [{
-    name: 'nickname',
-    extend: false,
-    entries: [
-        {
-            value: 'College',
-            synonyms: ['College']
-        },
-        {
-            value: 'Retirement',
-            synonyms: ['Retirement']
-        }
-    ]
-}];
+        console.log('recognizeWithoutEntities', this.init);
 
-var user_entities_body = {
-    sessionId: sessionId,
-    entities: user_entities
-};
-console.log('user_entities_body', user_entities_body);
+        var request = app.textRequest(context.message.text, {sessionId: sessionId});
 
-var user_entities_request = app.userEntitiesRequest(user_entities_body);
-
-user_entities_request.on('response', function(response) {
-    console.log('User entities response: ');
-    console.log(JSON.stringify(response, null, 4));
-
-    var request = app.textRequest(context.message.text, {sessionId: sessionId});
-
-    console.log('sessionId', sessionId);
-    request.on('response', function(response) {
-        console.log('Query response: ');
-        console.log(JSON.stringify(response, null, 4));
-        callback(null, {
-            intent: response.result.metadata.intentName,
-            score: response.result.score,
-            parameters: response.result.parameters
+        //console.log('sessionId', sessionId);
+        request.on('response', function(response) {
+            //console.log('Query response: ');
+            //console.log(JSON.stringify(response, null, 4));
+            callback(null, {
+                intent: response.result.metadata.intentName,
+                score: response.result.score,
+                parameters: response.result.parameters
+            });
         });
-    });
 
-    request.on('error', function(error) {
-        console.log(error);
-    });
+        request.on('error', function(error) {
+            console.log(error);
+        });
 
-    request.end();
-});
-
-user_entities_request.on('error', function(error) {
-    console.log(error);
-});
-
-user_entities_request.end();
-
-}
+        request.end();
+      }
+    }
 };
