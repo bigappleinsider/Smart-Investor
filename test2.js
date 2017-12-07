828c6923a36a3d10ddffa92941ed417c7cc91208
Vlad Vinnikov <bigappleinsider@users.noreply.github.com>
2017-12-06 16:21:22 -0800

    simulate API request for entities

--- test2.js
+++ test2.js
@@ -6,10 +6,13 @@ var apiai = require("apiai");
 var app = apiai(process.env.APIAI_TOKEN);
 
 module.exports = {
-    init: false,
     recognizeTextRequest: function(context, callback) {
         var sessionId = "dsfhdfg";
-        console.log('recognizeWithoutEntities', this.init);
+        console.log('recognizeTextRequest',
+        'context.privateConversationData', context.privateConversationData,
+        'context.conversationId', context.message.address.conversation.id
+      );
+
 
         var request = app.textRequest(context.message.text, {sessionId: sessionId});
 
@@ -29,51 +32,59 @@ module.exports = {
 
         request.end();
     },
+    getEntities: function(sessionId) {
+      return new Promise(function (resolve) {
+        var user_entities = [{
+            name: 'nickname',
+            extend: false,
+            entries: [
+                {
+                    value: 'College',
+                    synonyms: ['College']
+                },
+                {
+                    value: 'Retirement',
+                    synonyms: ['Retirement']
+                }
+            ]
+        }];
+        setTimeout(function(){
+          resolve(user_entities);
+        }, 3000);
+      });
+    },
     recognizeWithEntities: function(context, callback) {
-      console.log('recognizeWithEntities', this.init);
-      this.init = true;
+
+      context.privateConversationData['init'] = true;
       var sessionId = "dsfhdfg";
 
-      var user_entities = [{
-          name: 'nickname',
-          extend: false,
-          entries: [
-              {
-                  value: 'College',
-                  synonyms: ['College']
-              },
-              {
-                  value: 'Retirement',
-                  synonyms: ['Retirement']
-              }
-          ]
-      }];
+      this.getEntities(sessionId)
+        .then(function(user_entities) {
+            var user_entities_body = {
+                sessionId: sessionId,
+                entities: user_entities
+            };
+            //console.log('user_entities_body', user_entities_body);
 
-      var user_entities_body = {
-          sessionId: sessionId,
-          entities: user_entities
-      };
-      //console.log('user_entities_body', user_entities_body);
+            var user_entities_request = app.userEntitiesRequest(user_entities_body);
+            var that = this;
+            user_entities_request.on('response', function(response) {
+                //console.log('User entities response: ');
+                //console.log(JSON.stringify(response, null, 4));
 
-      var user_entities_request = app.userEntitiesRequest(user_entities_body);
-      var that = this;
-      user_entities_request.on('response', function(response) {
-          //console.log('User entities response: ');
-          //console.log(JSON.stringify(response, null, 4));
+                that.recognizeTextRequest(context, callback);
 
-          that.recognizeTextRequest(context, callback);
+            });
 
-      });
+            user_entities_request.on('error', function(error) {
+                console.log(error);
+            });
 
-      user_entities_request.on('error', function(error) {
-          console.log(error);
-      });
-
-      user_entities_request.end();
-
+            user_entities_request.end();
+          });
     },
     recognize: function(context, callback) {
-      if(!this.init) {
+      if(!context.privateConversationData['init']) {
         this.recognizeWithEntities(context, callback);
       }
       else {
